cc4cf7030565f94dbbf55bc4907bfa56872b0335
lewis267 <lewis267@purdue.edu>
2018-08-24 15:09:16 -0400

    Added an abstract definition of a playlist.

--- app/src/main/java/com/lewis267/musicplayer/Playlist.java
+++ app/src/main/java/com/lewis267/musicplayer/Playlist.java
@@ -1,4 +1,27 @@
 package com.lewis267.musicplayer;
 
-public class Playlist {
+/**
+ * Defines an abstract definition of
+ * a playlist element.
+ */
+public abstract class Playlist {
+
+    /**
+     * The current track from the playlist.
+     */
+    public Song CurrentSong;
+
+    /**
+     * Obtains the next song in the playlist
+     * and moves current forward.
+     * @return The new current track.
+     */
+    public abstract Song shiftNext();
+
+    /**
+     * Obtains the last song in the playlist
+     * and moves current backwards.
+     * @return The new current track.
+     */
+    public abstract Song shiftPrevious();
 }
